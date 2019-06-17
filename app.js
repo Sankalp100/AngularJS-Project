a0121d9eb4f140583c4fe9dba31dec8c8d48442e
Sankalp100 <sankalpgour123@gmail.com>
2019-06-17 16:29:01 +0530

    module-1

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
 (function () {
-     'use strict';
+    'use strict';
 
-    angular.module("NameCalculator", [])
+    angular.module("LunchCheck", [])
+        .controller('LunchCheckController', LunchCheackController);
 
-    .controller('NameCalculatorController', function($scope){
-        $scope.name = "";
-        $scope.totalValue = 0;
+    LunchCheackController.$inject = ['$scope'];
 
-        $scope.displayNumeric = function() {
-            var totalNameValue = calculateNumericForString($scope.name);
-            $scope.totalValue = totalNameValue; 
-        };
+    function LunchCheackController($scope) {
+        $scope.items = "";
+        $scope.message1 = "";
+        $scope.message2 = "";
+        $scope.messageEmpty = false;
 
-        function calculateNumericForString(string) {
-            var totalStringValue = 0;
-            for (var i = 0; i < string.length; i++){
-                totalStringValue += string.charCodeAt(i);
+        $scope.checkLunch = function(){
+            if ($scope.items.length === 0){
+                $scope.message1 = "Please enter data first";
+                $scope.messageEmpty = true;
+            }else {
+                var arrayItems = $scope.items.split(',');
+                if( arrayItems.length <= 3 ){
+                    $scope.message2 = "Enjoy!";
+                }else {
+                    $scope.message2 = "Too much!";
+                }
             }
-
-            return totalStringValue;
         }
-    });
+    }
+            
+
+
+
+            // $scope.displayNumeric = function() {
+            //     var totalNameValue = 
+            //     calculateNumericForString($scope.name);
+            //     $scope.totalValue = totalNameValue; 
+            // };
+
+            // function calculateNumericForString(string) {
+            //     var totalStringValue = 0;
+            //     for (var i = 0; i < string.length; i++){
+            //         totalStringValue += string.charCodeAt(i);
+            //     }
+
+            //     return totalStringValue;
+            // }
+        
 })();
